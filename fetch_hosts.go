8693425190ae841e54020dd96f888855fe3b37d2
Licoy <licoycn@gmail.com>
2022-06-19 11:26:38 +0800

    feat: 优化hosts的更新的逻辑为行对比

--- fetch_hosts.go
+++ fetch_hosts.go
@@ -4,13 +4,11 @@ import (
 	"bytes"
 	"embed"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
-	"regexp"
 	"strings"
 	"time"
 )
@@ -113,6 +111,13 @@ func ClientFetchHosts(url string) (err error) {
 		return
 	}
 
+	domains, err := getGithubDomains()
+	if err != nil {
+		return
+	}
+
+	localHostsLines := strings.Split(string(hostsBytes), "\n")
+
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode != http.StatusOK {
 		err = ComposeError("获取最新的hosts失败", err)
@@ -125,29 +130,41 @@ func ClientFetchHosts(url string) (err error) {
 		return
 	}
 
-	fetchHostsStr := strings.Trim(string(fetchHosts), "\n")
+	fetchHostsLines := strings.Split(string(fetchHosts), "\n")
 
-	mth, err := regexp.Compile(`# fetch-github-hosts begin(([\s\S])*.?)# fetch-github-hosts end`)
-	if err != nil {
-		err = ComposeError("创建内容正则匹配失败", err)
-		return
+	// clear local hosts github domain
+	hosts := &bytes.Buffer{}
+
+	for _, localLine := range localHostsLines {
+		line := strings.TrimSpace(localLine)
+		if line == "" || strings.HasPrefix(line, "#") {
+			hosts.WriteString(localLine)
+			hosts.WriteString("\n")
+			continue
+		}
+		var clearLine bool
+		for _, domain := range domains {
+			if strings.Contains(line, domain) {
+				clearLine = true
+				break
+			}
+		}
+		if !clearLine {
+			hosts.WriteString(localLine)
+			hosts.WriteString("\n")
+		}
 	}
 
-	if len(mth.FindStringSubmatch(fetchHostsStr)) == 0 {
-		err = errors.New("无效的远程hosts链接，未通过格式校验")
-		return
+	for _, fetchLine := range fetchHostsLines {
+		line := strings.TrimSpace(fetchLine)
+		if line == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+		hosts.WriteString(fetchLine)
+		hosts.WriteString("\n")
 	}
 
-	hosts := string(hostsBytes)
-
-	findStr := mth.FindStringSubmatch(hosts)
-	if len(findStr) > 0 {
-		hosts = strings.Replace(hosts, findStr[0], fetchHostsStr, 1)
-	} else {
-		hosts += "\n\n" + fetchHostsStr + "\n"
-	}
-
-	if err = ioutil.WriteFile(hostsPath, []byte(hosts), os.ModeType); err != nil {
+	if err = ioutil.WriteFile(hostsPath, hosts.Bytes(), os.ModeType); err != nil {
 		err = ComposeError("写入hosts文件失败，请用超级管理员身份启动本程序！", err)
 		return
 	}
@@ -158,15 +175,8 @@ func ClientFetchHosts(url string) (err error) {
 // ServerFetchHosts 服务端获取github最新的hosts并写入到对应文件及更新首页
 func ServerFetchHosts() (err error) {
 	execDir := AppExecDir()
-	fileData, err := GetExecOrEmbedFile(&domainsJson, "domains.json")
+	domains, err := getGithubDomains()
 	if err != nil {
-		err = ComposeError("读取文件domains.json错误", err)
-		return
-	}
-
-	var domains []string
-	if err = json.Unmarshal(fileData, &domains); err != nil {
-		err = ComposeError("domain.json解析失败", err)
 		return
 	}
 
@@ -221,5 +231,19 @@ func FetchHosts(domains []string) (hostsJson, hostsFile []byte, now string, err 
 	hostsFileData.WriteString("\n# update url: https://hosts.gitcdn.top/hosts.txt\n# fetch-github-hosts end\n\n")
 	hostsFile = hostsFileData.Bytes()
 	hostsJson, err = json.Marshal(hosts)
+	return
+}
+
+func getGithubDomains() (domains []string, err error) {
+	fileData, err := GetExecOrEmbedFile(&domainsJson, "domains.json")
+	if err != nil {
+		err = ComposeError("读取文件domains.json错误", err)
+		return
+	}
+
+	if err = json.Unmarshal(fileData, &domains); err != nil {
+		err = ComposeError("domain.json解析失败", err)
+		return
+	}
 	return
 }
